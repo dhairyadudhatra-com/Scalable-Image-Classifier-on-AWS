105b0fcb981b1263bee92a2ff93bca65584efbba
Dhairya Dudhatra <dhairyadudhatra@gmail.com>
2023-02-24 11:59:07 -0700

    Web-tier Pythonfile final version

--- webtier-app.py
+++ webtier-app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import boto3
 import base64
 import json
-
+import time
 app = Flask(__name__)
 
 sqs = boto3.client('sqs',region_name='us-east-1')
@@ -13,35 +13,42 @@ output_queue_url = sqs.get_queue_url(QueueName="app-web-result-queue")["QueueUrl
 @app.route('/', methods=["POST"])
 def process_file():
 
-    img_cont = request.files["myfile"].read()
-    imagename = request.form['filename']
+    img_cont = request.files["myfile"]
+    imagename = img_cont.filename
+    imagedata = img_cont.read()
     print("\n"+ imagename + " Received")
     
 
     if img_cont is None:
         return "No file in the request"
     
-    image_data = base64.b64encode(img_cont).decode('utf-8')
+    image_data = base64.b64encode(imagedata).decode('utf-8')
     message_body = {'image_data': image_data, 'filename': imagename}
     message_json = json.dumps(message_body)
+
     sqs.send_message(QueueUrl=input_queue_url, MessageBody=message_json)
-    print(imagename+ " uploaded in the queue\n")
-
+    #print(imagename+ " uploaded in the queue\n")
+    time.sleep(2)
     result = get_result(imagename)
-    return result, 200
+    return result,200 #result, 200
 
+output_dict = {}
 def get_result(imagename):
-    while True:
-        response = sqs.receive_message(QueueUrl=output_queue_url, MaxNumberOfMessages=1)
-        if 'Messages' in response:
-            message = response['Messages'][0]['Body']
-            dict = json.loads(message)
-            if imagename in dict:
+    if imagename in output_dict:
+        return output_dict[imagename]
+    else:
+        while True:
+            response = sqs.receive_message(QueueUrl=output_queue_url, MaxNumberOfMessages=1, WaitTimeSeconds=5)
+            if 'Messages' in response:
+                message = response['Messages'][0]['Body']
+                dict1 = json.loads(message)
+                print(dict1)
+                output_dict[list(dict1.keys())[0]] = list(dict1.values())[0]
                 sqs.delete_message(QueueUrl=output_queue_url,ReceiptHandle=response['Messages'][0]['ReceiptHandle'])
-                return dict[imagename]
-            else:
-                print("not found ... checking more messages")
-
+                
+            if imagename in output_dict:
+                return output_dict[imagename]
+                                
 
 if __name__ == '__main__' :
     app.run()
